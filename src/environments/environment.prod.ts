ea9e91a9c8d5b333640ebafde3c8ae9f7ea10642
Drako <dragon12xw@gmail.com>
2021-04-01 11:18:33 -0500

    git push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDqKUcVDQt-QIfzaXadvoPuKugSYUfeODM",
-    authDomain: "picth-tempo.firebaseapp.com",
+    apiKey: "xxxxxxxxxxxx",
+    authDomain: "picth-t",
     projectId: "picth-tempo",
-    storageBucket: "picth-tempo.appspot.com",
-    messagingSenderId: "84599358862",
-    appId: "1:84599358862:web:647277ff7638ae09f0d0df",
-    measurementId: "G-P2PP60BLMQ"
+    storageBucket: "picth-tempoxxxxxxx",
+    messagingSenderId: "84xxxxxxxxx",
+    appId: "1:8xxxxx",
+    measurementId: "xxxxxxx"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,21 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDqKUcVDQt-QIfzaXadvoPuKugSYUfeODM",
-    authDomain: "picth-tempo.firebaseapp.com",
+    apiKey: "xxxxxxxxxxxx",
+    authDomain: "picth-t",
     projectId: "picth-tempo",
-    storageBucket: "picth-tempo.appspot.com",
-    messagingSenderId: "84599358862",
-    appId: "1:84599358862:web:647277ff7638ae09f0d0df",
-    measurementId: "G-P2PP60BLMQ"
+    storageBucket: "picth-tempoxxxxxxx",
+    messagingSenderId: "84xxxxxxxxx",
+    appId: "1:8xxxxx",
+    measurementId: "xxxxxxx"
   }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
